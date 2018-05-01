229cc394c018f82b93a4f56295ddf89e0b137d0f
RalucaNicola <rnicola@esri.com>
2018-05-01 17:25:30 +0200

    Change style of intro page

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -17,36 +17,43 @@ export default class Menu extends React.Component<MenuProps, any> {
   render() {
     return (
       <div className={ "intro " + (this.props.webscene ? "low" : "") }>
-      <div className="description">
-        <h1 className="title">Edge rendering</h1>
-        <p>for city visualization</p>
-        <p>using #esriJS</p>
-      </div>
-      <div className="intro-container">
+        <div className="description">
+          <h1 className="title">Edge rendering</h1>
+          <p>For city visualizations:
+            <span className="urban"
+            onClick = {(event: any) => this.props.handleSceneChange("urban")}
+            > Helsinki old buildings</span>,
+            <span className="touristic"> Famous buildings in Paris</span>,
+            <span className="night"> Manhattan by night</span>,
+            <span className="old"> Old scene of Lyon</span>.
+          </p>
+          <p className="info"> using <a href="https://developers.arcgis.com/javascript/" target="_blank">ArcGIS API for JS </a>
+          and data from awesome contributors to <a href="https://livingatlas.arcgis.com/en/" target="_blank">The Living Atlas</a>.
+          Fork me on <a href="https://github.com/RalucaNicola/edge-rendering" target="_blank">Github</a>!</p>
+        </div>
         <div className="menu">
           <div className="menu-item">
-            <img src={urban} alt="urban cartography"
+            <img className="urban" src={urban} alt="urban cartography"
               onClick = {(event: any) => this.props.handleSceneChange("urban")}
             />
           </div>
           <div className="menu-item">
-            <img src={touristic} alt="tourist map"
+            <img className="touristic" src={touristic} alt="tourist map"
               onClick = {(event: any) => this.props.handleSceneChange("touristic")}
             />
           </div>
-          <div className="menu-item">
-            <img src={night} alt="Manhattan by night"
+          <div className="menu-item night">
+            <img className="night" src={night} alt="Manhattan by night"
               onClick = {(event: any) => this.props.handleSceneChange("night")}
             />
           </div>
-          <div className="menu-item">
-            <img src={old} alt="Old map of Lyon"
+          <div className="menu-item old">
+            <img className="old" src={old} alt="Old map of Lyon"
               onClick = {(event: any) => this.props.handleSceneChange("old")}
             />
           </div>
         </div>
       </div>
-    </div>
     );
   }
 }
